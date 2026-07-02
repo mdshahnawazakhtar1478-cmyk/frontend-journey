c8fcc8bb1fc7368e2bfec7a95e364f0f487d70d6
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-07-02 17:48:57 +0530

    Added comments for form.js files

--- week-2/day-2-notes/form-with-js/form.js
+++ week-2/day-2-notes/form-with-js/form.js
@@ -1,15 +1,21 @@
+// Get the form element using querySelector inbuilt function and store it in a variable called "form"
 const form = document.querySelector("form");
 
-form.addEventListener("submit", myFunction);
+// Add an event listener for the form submission (to get the values of the form fields)
+form.addEventListener("submit", myFunction); // (type: "submit", callback function: myFunction)
 
-function myFunction(event){
-
+// Callback function to handle the form data when the form is submitted
+function myFunction(event){ // (event parameter: it is passed to the callback function from addEventListener function)
+    // Prevent the default form submission behavior in browser (which would reload the page)
     event.preventDefault();
+
+    // Get the values of the form fields using their class names
     const fullName = document.querySelector(".fullName").value;
     const email = document.querySelector(".email").value;
     const phone = document.querySelector(".phone").value;
     const github = document.querySelector(".github").value;
     const message = document.querySelector(".message").value;
     
+    // Log the form data to the console as an object
     console.log({fullName, email, phone, github, message});
 }
