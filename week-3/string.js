dd3df790a2761d6542d78cb992abb0bea8d62ed4
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-12 17:28:44 +0530

    Add string.js file with personal information and console logs

---
+++ week-3/string.js
@@ -0,0 +1,24 @@
+console.log("Hello, World!");
+let myName="shahnawaz"
+console.log(myName)
+
+// let a= 50;
+// let b= 100;
+
+let title = "Md"
+console.log(title)
+let firstName = "Shahnawaz"
+console.log(firstName)
+let lastName = "Akhtar"
+console.log(lastName)
+let age = 29
+console.log(age)
+let github = "https://github.com/mdshahnawazakhtar1478-cmyk"
+console.log(github)
+let linkedin = "https://www.linkedin.com/in/md-shahnawaz-akhtar-00664719a/"
+console.log(linkedin)
+
+let fullName = title +" "+ firstName +" "+ lastName
+console.log(fullName)
+
+console.log("My name is " + fullName + ", my age is " + age + ", my github profile link is " + github + "and my linkedin profile link is " + linkedin)
