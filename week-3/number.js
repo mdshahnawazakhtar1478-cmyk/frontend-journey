f1b9fa52f04c9f77be01bd94be5e4198d6d61a3a
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-12 17:46:48 +0530

    Add number.js file with basic arithmetic operations and console logs

---
+++ week-3/number.js
@@ -0,0 +1,17 @@
+let a = 10;
+let b = 20;
+let c = 30;
+let d = 40;
+
+
+let sum = a+b+c+d;
+console.log("Sum of a, b, c, d is " + sum)
+
+let substarct = b-a;
+console.log("substarct of b, a is " + substarct)
+
+let multiple = b*c;
+console.log("multiple of b, c is " + multiple)
+
+let divide = d/b;
+console.log("Divided of d, b is " + divide)
