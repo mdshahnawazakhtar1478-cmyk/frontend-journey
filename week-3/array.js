ae94a06fcaccb45192e681b741725f9752512bce
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-13 18:15:52 +0530

    Add array.js with grocery items and console logs for item retrieval

---
+++ week-3/array.js
@@ -0,0 +1,26 @@
+let grocery = ["onion", "rice", "apple", "orange", 200, 500, 600]
+console.log(grocery)
+
+let type = typeof(grocery)
+console.log(type)
+
+let item_1 = grocery[0]
+console.log(item_1)
+
+let item_2 = grocery[1]
+console.log(item_2)
+
+let item_3 = grocery[2]
+console.log(item_3)
+
+let item_4 = grocery[3]
+console.log(item_4)
+
+let item_5 = grocery[4]
+console.log(item_5)
+
+let item_6 = grocery[5]
+console.log(item_6)
+
+let item_7 = grocery[6]
+console.log(item_7)
