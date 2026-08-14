a21f6e9a5bac477434c009a8086408cf4dec80d0
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-14 17:34:36 +0530

    Remove unused arithmetic functions from variable_function.js

--- week-3/variable_function.js
+++ week-3/variable_function.js
@@ -10,33 +10,3 @@ console.log(sum(5, 7))
 
 let Total = sum(15, 20);
 console.log(Total)
-
-/* substraction */
-
-function substraction(a, b){
-    let ab = a-b;
-    return ab;
-}
-
-let sub = substraction(a, b);
-console.log(sub(8, 5))
-
-/* multiplication */
-
-function multiplication(a, b){
-    let ab = a * b;
-    return ab;
-}
-
-let multi = multiplication(a, b);
-console.log(multi(4,5))
-
-/* division */
-
-function division(a, b){
-    let ab = a / b;
-    return ab;
-}
-
-let div = division(a, b);
-console.log(div(24, 4))
