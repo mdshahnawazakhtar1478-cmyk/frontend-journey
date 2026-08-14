67da3de35ca718cc679d32f83088ce875e18d57e
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-14 16:57:48 +0530

    created variable_function

---
+++ week-3/variable_function.js
@@ -0,0 +1,12 @@
+function sum(a, b){
+    let ab = a + b;
+    return ab;
+}
+
+console.log(sum(5, 6));
+console.log(sum(1000, 1500));
+console.log(sum(12, 9))
+console.log(sum(5, 7))
+
+let Total = sum(15, 20);
+console.log(Total)
