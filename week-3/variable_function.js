09b752e13a50430c4dc482c5da5a7cb160e624a1
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-14 17:40:13 +0530

    Add arithmetic functions: subtraction, multiplication, and division

--- week-3/variable_function.js
+++ week-3/variable_function.js
@@ -10,3 +10,33 @@ console.log(sum(5, 7))
 
 let Total = sum(15, 20);
 console.log(Total)
+
+/* substraction */
+
+function substraction(a, b){
+    let ab = a-b;
+    return ab;
+}
+
+let sub = substraction(6, 8);
+console.log(sub)
+
+/* multiplication */
+
+function multiplication(a, b){
+    let ab = a * b;
+    return ab;
+}
+
+let multi = multiplication(4, 5);
+console.log(multi)
+
+/* division */
+
+function division(a, b){
+    let ab = a / b;
+    return ab;
+}
+
+let div = division(24, 4);
+console.log(div)
