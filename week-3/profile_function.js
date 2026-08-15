dc2e46593609f48339b195d3c5d4d9ed3d1e91e7
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-15 17:54:26 +0530

    Add profile function to generate a user profile sentence

---
+++ week-3/profile_function.js
@@ -0,0 +1,12 @@
+function profile(firstName, lastName, age, profession){
+    let fullName = firstName + " " + lastName;
+    let sentance = "My Name is " + fullName + " I am " + age + " years old, and i am working in a company as a " + profession
+    return sentance ;
+}
+
+let profile_sentence = profile("shahnawaz", "akhtar", 28, "biller")
+console.log(profile_sentence)
+
+
+                                         
+
