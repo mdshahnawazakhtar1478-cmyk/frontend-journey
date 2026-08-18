04a0ba0f691e826062cbce49885bc886209b778d
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-18 11:55:27 +0530

    Add function declaration blueprint with examples for usage

---
+++ week-3/blueprint.js
@@ -0,0 +1,12 @@
+// function declaration blueprint
+function name(parameters){
+    // instruction 1
+    // instruction 2
+    // return final variable (optional)
+}
+
+// function calling without function return
+// name(arguments)
+
+// function calling with function return
+// declare variable = name(arguments)
