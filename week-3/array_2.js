e87cb7d04c1bb85544a3f0fd9bdbf1cce344c0f1
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-25 19:39:14 +0530

    Add initial array and console logs for fruits

---
+++ week-3/array_2.js
@@ -0,0 +1,20 @@
+let fruits = ["apple", "mango", "banana", "orange", "pine-apple"]
+console.log(fruits)
+
+let type = typeof(fruits)
+console.log(type)
+
+let item_1 = fruits[0]
+console.log(item_1)
+
+let item_2 = fruits[1]
+console.log(item_2)
+
+let item_3 = fruits[2]
+console.log(item_3)
+
+let item_4 = fruits[3]
+console.log(item_4)
+
+let item_5 = fruits[4]
+console.log(item_5)
