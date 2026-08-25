c428dda5f6954b80644e60aae39cbfef48d21c15
Md Shahnawaz Akhtar <mdshahnawazakhtar1478@gmail.com>
2026-08-25 19:44:36 +0530

    Add initial array and console logs for stationery items

---
+++ week-3/array_3.js
@@ -0,0 +1,20 @@
+let stationery = ["note-book", "pen", "pencil", "eraser", "scale"]
+console.log(stationery)
+
+let type = typeof(stationery)
+console.log(type)
+
+let item_1 = stationery[0]
+console.log(item_1)
+
+let item_2 = stationery[1]
+console.log(item_2)
+
+let item_3 = stationery[2]
+console.log(item_3)
+
+let item_4 = stationery[3]
+console.log(item_4)
+
+let item_5 = stationery[4]
+console.log(item_5)
